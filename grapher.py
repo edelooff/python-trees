50d9a3c5eaf64348637716e9762dcb0c78d20f7f
Elmer de Looff <elmer.delooff@gmail.com>
2020-11-03 23:49:39 +0100

    Updates style on TreeGrapher, and has it graph a tree based on a starting node.

--- grapher.py
+++ grapher.py
@@ -2,23 +2,22 @@ from collections import deque
 from colorsys import hls_to_rgb
 from functools import lru_cache
 
-from pydot import (
-    Dot,
-    Edge,
-    Node)
+from pydot import Dot, Edge, Node
 
 GRAPH_STYLE = {
-    'bgcolor': 'white',
-    'graph_type': 'graph',
-    'nodesep': 0.2,
-    'pad': "0.5, 0.5, 0, 0.5"}
+    "bgcolor": "white",
+    "graph_type": "graph",
+    "nodesep": 0.2,
+    "pad": "0.5, 0.5, 0, 0.5",
+}
 NODE_STYLE = {
-    'fillcolor': 'lightyellow',
-    'fontname': 'ubuntu mono bold',
-    'fontsize': 18,
-    'penwidth': 2,
-    'shape': 'circle',
-    'style': 'filled'}
+    "fillcolor": "lightyellow",
+    "fontname": "ubuntu mono bold",
+    "fontsize": 18,
+    "penwidth": 2,
+    "shape": "circle",
+    "style": "filled",
+}
 
 
 class EventAnimator:
@@ -27,24 +26,24 @@ class EventAnimator:
         self.base_name = base_name
 
     def graph_delete(self, topic, message):
-        marks = MarkedNodes({message['node']}, hue=0.9)
-        self._write_image(graph_avl_tree(message['tree'], marked_nodes=marks))
+        marks = MarkedNodes({message["node"]}, hue=0.9)
+        self._write_image(graph_avl_tree(message["tree"], marked_nodes=marks))
 
     def graph_insert(self, topic, message):
-        marks = MarkedNodes({message['node']}, hue=0.4)
-        self._write_image(graph_avl_tree(message['tree'], marked_nodes=marks))
+        marks = MarkedNodes({message["node"]}, hue=0.4)
+        self._write_image(graph_avl_tree(message["tree"], marked_nodes=marks))
 
     def graph_rebalanced(self, topic, message):
-        root = message['root']
+        root = message["root"]
         marks = MarkedNodes({root, root.left, root.right}, hue=0.6)
-        self._write_image(graph_avl_tree(message['tree'], marked_nodes=marks))
+        self._write_image(graph_avl_tree(message["tree"], marked_nodes=marks))
 
     def graph_rotation(self, topic, message):
-        marks = MarkedNodes(message['nodes'], hue=0.7)
-        self._write_image(graph_avl_tree(message['tree'], marked_nodes=marks))
+        marks = MarkedNodes(message["nodes"], hue=0.7)
+        self._write_image(graph_avl_tree(message["tree"].root, marked_nodes=marks))
 
     def _write_image(self, graph):
-        graph.write_png(f'{self.base_name}_{self._frame_count}.png')
+        graph.write_png(f"{self.base_name}_{self._frame_count}.png")
         self._frame_count += 1
 
 
@@ -60,23 +59,23 @@ class MarkedNodes:
     @staticmethod
     def _create_color(hue, lightness):
         rgb_color = (round(val * 255) for val in hls_to_rgb(hue, lightness, 1))
-        return '#{:02x}{:02x}{:02x}'.format(*rgb_color)
+        return "#{:02x}{:02x}{:02x}".format(*rgb_color)
 
 
 class TreeGrapher:
-    def __init__(self, tree, cache_size=256):
+    def __init__(self, root, cache_size=256):
+        if root is None:
+            raise ValueError("Cannot graph an empty tree")
         self._height = self._cached_node_height(cache_size)
-        self.tree = tree
+        self.root = root
 
     def create_graph(self, marked_nodes=None):
-        if self.tree.root is None:
-            raise ValueError('Cannot graph an empty tree')
         graph = Dot(**GRAPH_STYLE)
-        graph.set_edge_defaults(color='navy', penwidth=2)
+        graph.set_edge_defaults(color="navy", penwidth=2)
         graph.set_node_defaults(**NODE_STYLE)
         marked_nodes = marked_nodes or set()
-        self._draw_node(graph, self.tree.root, marked_nodes)
-        nodes = deque([self.tree.root])
+        self._draw_node(graph, self.root, marked_nodes)
+        nodes = deque([self.root])
         while nodes:
             node = nodes.popleft()
             if node.left:
@@ -96,21 +95,23 @@ class TreeGrapher:
         been updated yet. A cache ensures that determining the height of all
         subtrees in the tree only takes time on the order of the node count.
         """
+
         @lru_cache(maxsize=cache_size)
         def height(node):
             if node is None:
                 return -1
             return 1 + max(height(node.left), height(node.right))
+
         return height
 
     def _draw_divider(self, graph, node):
         """Draws a vertical divider to distinguish left/right child nodes."""
-        marker_style = {'label': '', 'width': 0, 'height': 0, 'style': 'invis'}
+        marker_style = {"label": "", "width": 0, "height": 0, "style": "invis"}
         source = node.value
         for _ in range(self._height(node)):
-            label = f':{source}'
+            label = f":{source}"
             graph.add_node(Node(label, **marker_style))
-            graph.add_edge(Edge(source, label, style='invis', weight=100))
+            graph.add_edge(Edge(source, label, style="invis", weight=100))
             source = label
 
     def _draw_node(self, graph, node, marked_nodes):
@@ -124,9 +125,9 @@ class TreeGrapher:
         else:
             graph.add_node(Node(node.value))
         if node.parent:
-            style = {'penwidth': 4 if self._tallest_sibling(node) else 2}
+            style = {"penwidth": 4 if self._tallest_sibling(node) else 2}
             if node in marked_nodes and node.parent in marked_nodes:
-                style['color'] = marked_nodes.edge_color
+                style["color"] = marked_nodes.edge_color
             graph.add_edge(Edge(node.parent.value, node.value, **style))
 
     def _tallest_sibling(self, node):
@@ -138,5 +139,5 @@ class TreeGrapher:
 
 
 def graph_avl_tree(tree, marked_nodes=None):
-    grapher = TreeGrapher(tree)
+    grapher = TreeGrapher(tree.root)
     return grapher.create_graph(marked_nodes=marked_nodes)

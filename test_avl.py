70b6d1b22da6bcc08a794a375b3c6a72c082560b
Elmer de Looff <elmer.delooff@gmail.com>
2021-04-18 20:21:35 +0200

    Adds verifier for AVL invariants, for easier comprehensive tests.

--- test_avl.py
+++ test_avl.py
@@ -1,6 +1,41 @@
 import pytest
 
 
+def tree_height(node):
+    if node is None:
+        return 0
+    return 1 + max(tree_height(node.left), tree_height(node.right))
+
+
+def assert_avl_invariants(node):
+    """Asserts the AVL tree invariants for the given node and all child nodes
+
+    - Asserts that the node's balance is between -1 and +1
+    - Asserts that the balance is equal to the attached subtree height difference
+    - Asserts that the left child is smaller, right child is larger
+    """
+    assert node.balance in {-1, 0, 1}
+    assert node.balance == tree_height(node.right) - tree_height(node.left)
+    if node.left is not None:
+        assert node.left.value < node.value
+        assert_avl_invariants(node.left)
+    if node.right is not None:
+        assert node.value < node.right.value
+        assert_avl_invariants(node.right)
+
+
+def pre_order(tree):
+    """Returns a pre-order tree traversal as a list."""
+
+    def _traversal(node):
+        if node is not None:
+            yield node.value
+            yield from _traversal(node.left)
+            yield from _traversal(node.right)
+
+    return list(_traversal(tree.root))
+
+
 @pytest.fixture
 def Tree():
     from avl import AVLTree
@@ -51,32 +86,23 @@ def test_double_insert_exception(tree):
 @pytest.mark.parametrize(
     "insert, expected",
     [
-        ((1, 2, 3), (2, 1, 3)),  # rotate left
-        ((3, 2, 1), (2, 1, 3)),  # rotate right
-        ((1, 3, 2), (2, 1, 3)),  # rotate right-left
-        ((3, 1, 2), (2, 1, 3)),  # rotate left-right
+        pytest.param([1, 2, 3], [2, 1, 3], id="left rotation"),
+        pytest.param([3, 2, 1], [2, 1, 3], id="right rotation"),
+        pytest.param([1, 3, 2], [2, 1, 3], id="right-left rotation"),
+        pytest.param([3, 1, 2], [2, 1, 3], id="left-right rotation"),
     ],
 )
 def test_basic_rotations(Tree, insert, expected):
     """Tests basic tree rotations and resulting balance."""
     tree = Tree(insert)
-    root, left, right = expected
-    assert tree.root.value == root
-    assert tree.root.left.value == left
-    assert tree.root.right.value == right
-    # Tree should be fully balanced
-    assert tree.root.balance == 0
-    assert tree.root.left.balance == 0
-    assert tree.root.right.balance == 0
+    assert_avl_invariants(tree.root)
+    assert pre_order(tree) == expected
 
 
 def test_double_rotation(Tree):
     tree = Tree(5, 4, 3, 2, 1)
-    assert tree.root.value == 4
-    assert tree.root.right.value == 5
-    assert tree.root.left.value == 2
-    assert tree.root.left.left.value == 1
-    assert tree.root.left.right.value == 3
+    assert_avl_invariants(tree.root)
+    assert pre_order(tree) == [4, 2, 1, 3, 5]
 
 
 def test_delete_nonexisting(tree):
@@ -95,33 +121,33 @@ def test_delete_root(tree):
 
 
 @pytest.mark.parametrize(
-    "insert, delete, exp_root, exp_balance",
+    "insert, delete, expected",
     [
-        ((1, 2), 1, 2, 0),
-        ((1, 2), 2, 1, 0),
-        ((4, 3), 3, 4, 0),
-        ((4, 3), 4, 3, 0),
-        ((2, 1, 3), 1, 2, 1),
-        ((2, 1, 3), 2, 1, 1),  # Implementation-defined, prefer left node
-        ((2, 1, 3), 3, 2, -1),
+        pytest.param([4, 6], 4, [6], id="root of right-leaning line"),
+        pytest.param([4, 6], 6, [4], id="leaf of right-leaning line"),
+        pytest.param([4, 2], 4, [2], id="root of left-leaning line"),
+        pytest.param([4, 2], 2, [4], id="leaf of left-leaning line"),
+        pytest.param([4, 2, 6], 2, [4, 6], id="left leaf of V-tree"),
+        pytest.param([4, 2, 6], 6, [4, 2], id="right leaf of V-tree"),
     ],
 )
-def test_delete_simple(Tree, insert, delete, exp_root, exp_balance):
+def test_delete_trivial(Tree, insert, delete, expected):
     """Tests deletion of root/leaf nodes requiring no rotation."""
     tree = Tree(insert)
     tree.delete(delete)
     assert delete not in tree
-    assert tree.root.value == exp_root
-    assert tree.root.balance == exp_balance
+    assert pre_order(tree) == expected
+    assert_avl_invariants(tree.root)
 
 
 @pytest.mark.parametrize(
     "insert, deletes, expected",
     [
-        ((3, 2, 4, 1, 5), (2, 4), (3, 1, 5)),
-        ((3, 2, 4, 1, 5), (3, 4), (2, 1, 5)),
-        ((3, 1, 5, 2, 4), (1, 5), (3, 2, 4)),
-        ((4, 2, 6, 1, 3, 5), (4, 1, 6), (3, 2, 5)),
+        ([2, 1, 3], [2], [1, 3]),  # Implementation-defined, prefer left node
+        ([3, 2, 4, 1, 5], [2, 4], [3, 1, 5]),
+        ([3, 2, 4, 1, 5], [3, 4], [2, 1, 5]),
+        ([3, 1, 5, 2, 4], [1, 5], [3, 2, 4]),
+        ([4, 2, 6, 1, 3, 5], [4, 1, 6], [3, 2, 5]),
     ],
 )
 def test_delete_attach_without_rotation(Tree, insert, deletes, expected):
@@ -130,14 +156,8 @@ def test_delete_attach_without_rotation(Tree, insert, deletes, expected):
     for key in deletes:
         tree.delete(key)
     assert key not in tree
-    root, left, right = expected
-    assert tree.root.value == root
-    assert tree.root.left.value == left
-    assert tree.root.right.value == right
-    # Tree should be fully balanced
-    assert tree.root.balance == 0
-    assert tree.root.left.balance == 0
-    assert tree.root.right.balance == 0
+    assert pre_order(tree) == expected
+    assert_avl_invariants(tree.root)
 
 
 def test_delete_reattach(Tree):
@@ -145,41 +165,36 @@ def test_delete_reattach(Tree):
     tree = Tree(3, 2, 4, 1, 5)
     tree.delete(3)
     assert 3 not in tree
-    assert tree.root.value == 2
-    assert tree.root.right.value == 4
-    assert tree.root.left.value == 1
+    assert pre_order(tree) == [2, 1, 4, 5]
+    assert_avl_invariants(tree.root)
 
 
 @pytest.mark.parametrize(
     "insert, delete, expected",
     [
-        ((4, 2, 6, 7), 2, (6, 4, 7)),  # rotate left
-        ((4, 2, 6, 1), 6, (2, 1, 4)),  # rotate right
-        ((4, 2, 6, 5), 2, (5, 4, 6)),  # rotate right-left
-        ((4, 2, 6, 3), 6, (3, 2, 4)),  # rotate left-right
+        pytest.param([4, 2, 6, 7], 2, [6, 4, 7], id="left rotation"),
+        pytest.param([4, 2, 6, 5, 7], 2, [6, 4, 5, 7], id="left (balanced) rotation"),
+        pytest.param([4, 2, 6, 1], 6, [2, 1, 4], id="right rotation"),
+        pytest.param([4, 2, 6, 1, 3], 6, [2, 1, 4, 3], id="right (balanced) rotation"),
+        pytest.param([4, 2, 6, 5], 2, [5, 4, 6], id="right-left rotation"),
+        pytest.param([4, 2, 6, 3], 6, [3, 2, 4], id="left-right rotation"),
     ],
 )
 def test_delete_single_rotation(Tree, insert, delete, expected):
     """Tests deletion resulting in a single rotation."""
     tree = Tree(insert)
     tree.delete(delete)
-    root, left, right = expected
-    assert tree.root.value == root
-    assert tree.root.left.value == left
-    assert tree.root.right.value == right
-    # Tree should be fully balanced
-    assert tree.root.balance == 0
-    assert tree.root.left.balance == 0
-    assert tree.root.right.balance == 0
+    assert delete not in tree
+    assert pre_order(tree) == expected
+    assert_avl_invariants(tree.root)
 
 
 def test_delete_double_rotation(Tree):
     """Tests deletion resulting in a double rotation."""
     tree = Tree(16, 8, 24, 4, 12, 20, 28, 2, 6, 10, 32, 1, 3)
     tree.delete(20)
+    assert 20 not in tree
     assert tree.root.value == 8
     assert tree.root.left.value == 4
     assert tree.root.right.value == 16
-    assert tree.root.balance == 0
-    assert tree.root.left.balance == -1
-    assert tree.root.right.balance == 0
+    assert_avl_invariants(tree.root)

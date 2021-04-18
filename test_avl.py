ed51dcc6eed96bd970b57149b5cca9ffd9900d85
Elmer de Looff <elmer.delooff@gmail.com>
2021-04-18 13:22:34 +0200

    Reformats tests and adds checks for absence of deleted keys.

--- test_avl.py
+++ test_avl.py
@@ -4,6 +4,7 @@ import pytest
 @pytest.fixture
 def Tree():
     from avl import AVLTree
+
     return AVLTree
 
 
@@ -48,12 +49,13 @@ def test_double_insert_exception(tree):
 
 
 @pytest.mark.parametrize(
-    'insert, expected', [
+    "insert, expected",
+    [
         ((1, 2, 3), (2, 1, 3)),  # rotate left
         ((3, 2, 1), (2, 1, 3)),  # rotate right
         ((1, 3, 2), (2, 1, 3)),  # rotate right-left
         ((3, 1, 2), (2, 1, 3)),  # rotate left-right
-    ]
+    ],
 )
 def test_basic_rotations(Tree, insert, expected):
     """Tests basic tree rotations and resulting balance."""
@@ -88,11 +90,13 @@ def test_delete_root(tree):
     """Tests deletion of singular root node."""
     tree.insert(1)
     tree.delete(1)
+    assert 1 not in tree
     assert tree.root is None
 
 
 @pytest.mark.parametrize(
-    'insert, delete, exp_root, exp_balance', [
+    "insert, delete, exp_root, exp_balance",
+    [
         ((1, 2), 1, 2, 0),
         ((1, 2), 2, 1, 0),
         ((4, 3), 3, 4, 0),
@@ -100,29 +104,32 @@ def test_delete_root(tree):
         ((2, 1, 3), 1, 2, 1),
         ((2, 1, 3), 2, 1, 1),  # Implementation-defined, prefer left node
         ((2, 1, 3), 3, 2, -1),
-    ]
+    ],
 )
 def test_delete_simple(Tree, insert, delete, exp_root, exp_balance):
     """Tests deletion of root/leaf nodes requiring no rotation."""
     tree = Tree(insert)
     tree.delete(delete)
+    assert delete not in tree
     assert tree.root.value == exp_root
     assert tree.root.balance == exp_balance
 
 
 @pytest.mark.parametrize(
-    'insert, deletes, expected', [
+    "insert, deletes, expected",
+    [
         ((3, 2, 4, 1, 5), (2, 4), (3, 1, 5)),
         ((3, 2, 4, 1, 5), (3, 4), (2, 1, 5)),
         ((3, 1, 5, 2, 4), (1, 5), (3, 2, 4)),
         ((4, 2, 6, 1, 3, 5), (4, 1, 6), (3, 2, 5)),
-    ]
+    ],
 )
 def test_delete_attach_without_rotation(Tree, insert, deletes, expected):
     """Tests selective pruning of nodes and reattaching them to parents."""
     tree = Tree(insert)
     for key in deletes:
         tree.delete(key)
+    assert key not in tree
     root, left, right = expected
     assert tree.root.value == root
     assert tree.root.left.value == left
@@ -137,6 +144,7 @@ def test_delete_reattach(Tree):
     """Removing the root from a 2-depth V should reattach nodes correctly."""
     tree = Tree(3, 2, 4, 1, 5)
     tree.delete(3)
+    assert 3 not in tree
     assert tree.root.value == 2
     assert tree.root.right.value == 4
     assert tree.root.left.value == 1
@@ -146,18 +154,20 @@ def test_delete_reattach_right(Tree):
     """Removing the root from a 2-depth V should reattach nodes correctly."""
     tree = Tree(3, 2, 4, 5)
     tree.delete(3)
+    assert 3 not in tree
     assert tree.root.value == 4
     assert tree.root.right.value == 5
     assert tree.root.left.value == 2
 
 
 @pytest.mark.parametrize(
-    'insert, delete, expected', [
+    "insert, delete, expected",
+    [
         ((4, 2, 6, 7), 2, (6, 4, 7)),  # rotate left
         ((4, 2, 6, 1), 6, (2, 1, 4)),  # rotate right
         ((4, 2, 6, 5), 2, (5, 4, 6)),  # rotate right-left
         ((4, 2, 6, 3), 6, (3, 2, 4)),  # rotate left-right
-    ]
+    ],
 )
 def test_delete_single_rotation(Tree, insert, delete, expected):
     """Tests deletion resulting in a single rotation."""

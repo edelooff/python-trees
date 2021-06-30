b5c0addc7eea0e20199185fdd4077da21679d4ab
Elmer de Looff <elmer.delooff@gmail.com>
2021-07-01 00:22:27 +0200

    Adds Red-Black tree with balanced inserts.

---
+++ src/sherwood/trees/redblack.py
@@ -0,0 +1,132 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum, auto
+from itertools import takewhile
+from typing import Any, Iterator, Optional, cast, final
+
+from .base import BinaryNode, Tree
+
+
+class Color(Enum):
+    red = auto()
+    black = auto()
+    __inverted__ = {red: black, black: red}
+
+    @property
+    def inverse(self) -> Color:
+        return Color(self.__inverted__[self.value])
+
+
+@final
+@dataclass(eq=False)
+class RBNode(BinaryNode):
+    color: Color = Color.red
+    left: Optional[RBNode] = None
+    right: Optional[RBNode] = None
+
+
+class RedBlackTree(Tree):
+    root: Optional[RBNode]
+
+    def delete(self, key: Any) -> None:
+        raise NotImplementedError
+
+    def insert(self, key: Any) -> RBNode:
+        if self.root is None:
+            self.root = RBNode(key)
+            self.publish("insert", self.root)
+            return self.root
+
+        lineage = [node := self.root]
+        while True:
+            if key == node.value:
+                raise ValueError(f"duplicate key {key!r} in Red-Black Tree")
+            elif key < node.value:
+                if node.left is not None:
+                    lineage.append(node := node.left)
+                    continue
+                node.left = new = RBNode(key)
+            else:
+                if node.right is not None:
+                    lineage.append(node := node.right)
+                    continue
+                node.right = new = RBNode(key)
+            self.publish("insert", new)
+            self.rebalance(new, reversed(lineage))
+            return new
+
+    def rebalance(self, node: RBNode, lineage: Iterator[RBNode]) -> None:
+        """Rebalances the red-black tree after insertion of a new node."""
+        # lineage traversal includes the 'parent is black' termination condition
+        for parent in takewhile(lambda node: node.color is not Color.black, lineage):
+            if parent is self.root:
+                parent.color = Color.black
+                break
+            grandparent = next(lineage)
+            if has_all_red_children(grandparent):
+                node = grandparent
+                invert_color(node, cast(RBNode, node.left), cast(RBNode, node.right))
+                continue
+            # Determine rotation necessary to rebalance tree
+            if node is parent.left:
+                same = parent is grandparent.left
+                rotate = self.rotate_right if same else self.rotate_right_left
+            else:
+                same = parent is grandparent.right
+                rotate = self.rotate_left if same else self.rotate_left_right
+            # Attach rebalanced subtree to grandparent, or tree root
+            subtree = rotate(grandparent, parent, node)
+            if (anchor := next(lineage, None)) is None:
+                self.root = subtree
+            elif anchor.left is grandparent:
+                anchor.left = subtree
+            else:
+                anchor.right = subtree
+            self.publish("balanced", subtree, subtree.left, subtree.right)
+            if anchor is None or anchor.color is Color.black:
+                break
+            node = anchor
+
+    def rotate_left(self, root: RBNode, pivot: RBNode, tail: RBNode) -> RBNode:
+        """Hoists the pivot up to to be the new root of the given triple."""
+        self.publish("rotate.left", root, pivot, tail)
+        root.right, pivot.left = pivot.left, root
+        invert_color(root, pivot)
+        return pivot
+
+    def rotate_right(self, root: RBNode, pivot: RBNode, tail: RBNode) -> RBNode:
+        """Hoists the pivot up to to be the new root of the given triple."""
+        self.publish("rotate.right", root, pivot, tail)
+        root.left, pivot.right = pivot.right, root
+        invert_color(root, pivot)
+        return pivot
+
+    def rotate_left_right(self, root: RBNode, outer: RBNode, pivot: RBNode) -> RBNode:
+        """Hoists the pivot up to to be the new root of the given triple."""
+        self.publish("rotate.leftright", root, pivot, outer)
+        outer.right, root.left = pivot.left, pivot.right
+        pivot.left, pivot.right = outer, root
+        invert_color(root, pivot)
+        return pivot
+
+    def rotate_right_left(self, root: RBNode, outer: RBNode, pivot: RBNode) -> RBNode:
+        """Hoists the pivot up to to be the new root of the given triple."""
+        self.publish("rotate.rightleft", root, pivot, outer)
+        root.right, outer.left = pivot.left, pivot.right
+        pivot.left, pivot.right = root, outer
+        invert_color(root, pivot)
+        return pivot
+
+
+def has_all_red_children(node: RBNode) -> bool:
+    return (
+        node.left is not None
+        and node.right is not None
+        and node.left.color is node.right.color is Color.red
+    )
+
+
+def invert_color(*nodes: RBNode) -> None:
+    for node in nodes:
+        node.color = node.color.inverse
---
+++ tests/test_redblack.py
@@ -0,0 +1,137 @@
+from __future__ import annotations
+
+import random
+
+import pytest
+
+from sherwood.trees.redblack import Color, RedBlackTree
+
+
+def assert_invariants(node):
+    """Asserts the Red-Black tree invariants for the given node and all child nodes
+
+    - Asserts that the node's color is either `Color.red` or `Color.black`;
+    - Asserts children of a red colored node are both colored black;
+    - Asserts that all paths from the node down to any of the leaves passes
+      through an equal number of black-colored nodes;
+    - Asserts that the left child (if any) is smaller, and the inverse for the right
+    """
+    if node is None:
+        return
+
+    assert node.color in Color
+    if node.color is Color.red:
+        assert node_is_black(node.left)
+        assert node_is_black(node.right)
+
+    # All paths have the same number of black-colored nodes in them (black-depth)
+    paths = list(paths_to_leaf(node))
+    assert len(set(map(black_depth, paths))) == 1
+
+    # Base binary tree validation and recursive checks down
+    if node.left is not None:
+        assert node.left.value < node.value
+        assert_invariants(node.left)
+    if node.right is not None:
+        assert node.value < node.right.value
+        assert_invariants(node.right)
+
+
+def black_depth(path):
+    """Returns the count of black nodes in an iterable."""
+    return sum(1 for node in path if node.color is Color.black)
+    return sum(map(node_is_black, path))
+
+
+def node_is_black(node):
+    """Returns whether a node is black, with NIL nodes (None) considered black."""
+    return node is None or node.color is Color.black
+
+
+def paths_to_leaf(node):
+    def _dfs(node, stack):
+        if node is None:
+            yield stack
+        else:
+            yield from _dfs(node.left, stack + (node,))
+            yield from _dfs(node.right, stack + (node,))
+
+    yield from _dfs(node, ())
+
+
+def pre_order(tree):
+    """Returns a pre-order tree traversal as a list."""
+
+    def _traversal(node):
+        if node is not None:
+            yield node.value
+            yield from _traversal(node.left)
+            yield from _traversal(node.right)
+
+    return list(_traversal(tree.root))
+
+
+def test_tree_insert():
+    tree = RedBlackTree()
+    tree.insert(5)
+    assert 5 in tree
+
+
+def test_instantiate_from_list():
+    tree = RedBlackTree([1, 2])
+    assert 1 in tree
+    assert 2 in tree
+
+
+def test_instantiate_from_iter():
+    tree = RedBlackTree(range(1, 3))
+    assert 1 in tree
+    assert 2 in tree
+
+
+def test_instantiate_from_string():
+    tree = RedBlackTree("abc")
+    assert "a" in tree
+    assert "b" in tree
+    assert "c" in tree
+
+
+def test_double_insert_exception():
+    tree = RedBlackTree([5])
+    with pytest.raises(ValueError):
+        tree.insert(5)
+
+
+@pytest.mark.parametrize(
+    "insert, expected_root_color, expected_pre_order",
+    [
+        pytest.param([4, 2], Color.black, [4, 2], id="left heavy"),
+        pytest.param([4, 6], Color.black, [4, 6], id="right heavy"),
+        pytest.param([4, 2, 6, 1], Color.red, [4, 2, 1, 6], id="row color swap"),
+        pytest.param([4, 2, 6, 3], Color.red, [4, 2, 3, 6], id="row color swap"),
+        pytest.param([4, 2, 6, 5], Color.red, [4, 2, 6, 5], id="row color swap"),
+        pytest.param([4, 2, 6, 7], Color.red, [4, 2, 6, 7], id="row color swap"),
+    ],
+)
+def test_rotationless_recoloring(insert, expected_root_color, expected_pre_order):
+    """Tests recoloring of tree nodes without requiring rotations."""
+    tree = RedBlackTree(insert)
+    assert_invariants(tree.root)
+    assert tree.root.color is expected_root_color
+    assert pre_order(tree) == expected_pre_order
+
+
+@pytest.mark.parametrize(
+    "insert, expected",
+    [
+        pytest.param([1, 2, 3], [2, 1, 3], id="left rotation"),
+        pytest.param([3, 2, 1], [2, 1, 3], id="right rotation"),
+        pytest.param([1, 3, 2], [2, 1, 3], id="right-left rotation"),
+        pytest.param([3, 1, 2], [2, 1, 3], id="left-right rotation"),
+    ],
+)
+def test_basic_rotations(insert, expected):
+    """Tests basic tree rotations."""
+    tree = RedBlackTree(insert)
+    assert_invariants(tree.root)
+    assert pre_order(tree) == expected
